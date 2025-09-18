6c67fb20576f11a244a7cb58f18207fac0e681a9
Abah Dominic <constantx003@gmail.com>
2025-09-18 15:17:16 +0100

    Webpage showing

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,13 @@
-const utilities = require("../utilities/")
-const baseController = {}
+const utilities = require("../utilities");
+// const upgrades = require('../utilities');
+// const hero = require('../utilities');
+const baseController = {};
 
 baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
-}
+  const nav = await utilities.getNav();
+  const hero = utilities.getHero();
+  const upgrades = utilities.upgrades;
+  res.render("index", {title: "Home", nav, hero, upgrades});
+};
 
-module.exports = baseController
+module.exports = baseController;
--- utilities/index.js
+++ utilities/index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve "upgrades" folder as static files
+app.use('/upgrades', express.static(path.join(process.cwd(), 'public')));
+app.use('/hero', express.static(path.join(process.cwd(), 'public')));
+
 const invModel = require("../models/inventory-model")
 const Util = {}
 
@@ -22,6 +30,21 @@ Util.getNav = async function (req, res, next) {
   })
   list += "</ul>"
   return list
+};
+Util.getHero = function (req, res, next) {
+   const hero = {
+            title: 'Welcome to CSE Motors!',
+            alt: 'Delorean',
+            image: '/images/vehicles/delorean.jpg'
+        }
+    return hero;
 }
+
+Util.upgrades = [
+    {upgrade: '/images/upgrades/flux-cap.png', alt: 'Flux Capacitor'},
+    {upgrade: '/images/upgrades/flame.jpg', alt: 'Flame Decals'},
+    {upgrade: '/images/upgrades/bumper-sticker.jpg', alt: 'Bumper Stickers'},
+    {upgrade: '/images/upgrades/hub-cap.jpg', alt: 'Hub Caps'}
+  ];
 
 module.exports = Util
